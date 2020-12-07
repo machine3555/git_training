7eff3e53b61bd6faa1b6073d3a6a2e749307adcc
machine3555 <50790686+machine3555@users.noreply.github.com>
2020-12-07 13:54:19 +0530

    Add files via upload

---
+++ OneTOManyDataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace one_many
+{
+    internal class OneTOManyDataContext
+    {
+        public OneTOManyDataContext()
+        {
+        }
+
+        public object Orders { get; internal set; }
+
+        internal void savechanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
